55209c002d0778946d78a31f4d0e880e5f908c34
Eloisamarquez <60202894+Eloisamarquez@users.noreply.github.com>
2020-01-23 23:25:28 -0600

    refactor and added gen_vertical_csv function

--- new_puf_gen_csv.py
+++ new_puf_gen_csv.py
@@ -1,7 +1,26 @@
 import numpy as np
 import csv
 
-def gen_horizontal_csv(mu, sigma):
+
+def gen_vertical_csv(stages=32, mu=10, sigma=0.1):
+    with open('arbpuf_old_stage-per-row.csv', 'w', newline='') as csvfile:
+        fieldnames = ['stage', 'r_i', 's_i', 't_i', 'u_i', 'delta^0_i', 'delta^1_i']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for stage_number in range(1, stages + 1):
+            r = np.random.normal(mu, sigma)
+            s = np.random.normal(mu, sigma)
+            t = np.random.normal(mu, sigma)
+            u = np.random.normal(mu, sigma)
+            delta_0 = t - u
+            delta_1 = s - r
+
+            row = dict(zip(fieldnames, [stage_number, r, s, t, u, delta_0, delta_1]))
+            writer.writerow(row)
+
+
+def gen_horizontal_csv(stages=32, mu=10, sigma=0.1):
     stage=['stage']
     r=['r']
     s=['s']
@@ -10,7 +29,7 @@ def gen_horizontal_csv(mu, sigma):
     delta0=['delta0']
     delta1=['delta1']
 
-    for stage_number in range(1, n + 1):
+    for stage_number in range(1, stages + 1):
         stage.append(stage_number)
         r.append(np.random.normal(mu, sigma))
         s.append(np.random.normal(mu, sigma))
@@ -19,11 +38,10 @@ def gen_horizontal_csv(mu, sigma):
         delta0.append(t[stage_number] - u[stage_number])
         delta1.append(s[stage_number] - r[stage_number])
 
-    with open('test.csv', 'w', newline='') as csvfile:
+    with open('arbpuf.csv', 'w', newline='') as csvfile:
         csvwriter = csv.writer(csvfile, delimiter=',')
         csvwriter.writerows([stage, delta0, delta1, r, s, t, u])
 
 
-mu, sigma = 10, 0.1 # mean and standard deviation
-n = 32
-gen_horizontal_csv(mu, sigma)
+if __name__ == "__main__":
+    gen_horizontal_csv()
